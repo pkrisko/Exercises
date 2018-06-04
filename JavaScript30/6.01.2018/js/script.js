bb553822ce80e3af6587e2e626e6e827ea096ebb
prisko <patrick.krisko@gmail.com>
2018-06-04 12:00:13 -0400

    JavaScript30 Day 2: Flicker Bug Fix

--- JavaScript30/6.01.2018/js/script.js
+++ JavaScript30/6.01.2018/js/script.js
@@ -1,41 +1,55 @@
 const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
-const hand = document.querySelector('.hand');
+const allHands = document.querySelectorAll('.hand');
 
+/**
+ * Update the clock displayed on the screen based on values obtained from the
+ * JavaScript date class. Accounts for a bug which occurs when the second hand
+ * strikes 0.
+ */
 function setDate() {
     const now = new Date();
-    const seconds = now.getSeconds();
-    console.log(seconds);
-    // Debug the transition spaz every minute at top
-    if (seconds === 59 || seconds === 0) {
-        console.log("flagging");
-        hand.removeAttribute("transition");
-    } else {
-        hand.style.transition = `transition: all 0.05s`;
-    }
     setSecond(now);
     setHour(now);
     setMinute(now);
+    /* If seconds are 0, and the seconds hand is at the top, the transition needs
+     *  to be removed. Otherwise, the transition rotates counterclockwise, giving
+     * the appearance of a flicker. Removing this transition at this moment gets
+     * rid of this effect.
+     * NOTE: setting transition to '' will default the element back to the
+     * original style sheet styling, aka "all 0.05s;" */
+    const transitionFunc =  (now.getSeconds() === 0) ? (hand) => {hand.style.transition = 'none'} : (hand) => {hand.style.transition = ''};
+    allHands.forEach(transitionFunc);
 }
 
+/**
+ * Set the degree of the seconds hand. Called once every second by setDate().
+ * @param {*} now Current Date
+ */
 function setSecond(now) {
-    const seconds = now.getSeconds();
     // Translate seconds into angle at 1/60 of clock
-    const secondsDegrees = (seconds / 60) * 360 + 90; 
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    const secondsDegrees = (now.getSeconds() / 60) * 360 + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`; // <- backticks
 }
 
+/**
+ * Set the degree of the minutes hand. Called once every second by setDate().
+ * @param {*} now Current Date
+ */
 function setMinute(now) {
-    const minutes = now.getMinutes();
-    const minutesDegrees = (minutes / 60) * 360 + 90;
-    minsHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    const minutesDegrees = (now.getMinutes() / 60) * 360 + 90;
+    minsHand.style.transform = `rotate(${minutesDegrees}deg)`; // <- backticks
 }
 
+/**
+ * Set the degree of the hour hand. Called once every second by setDate().
+ * @param {*} now
+ */
 function setHour(now) {
-    const hours = now.getHours();
-    const hoursDegrees = (hours / 12) * 360 + 90;
-    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    // Note 12 hour clock. Can easily be changed to military by dividing by 24 instead.
+    const hoursDegrees = ( now.getHours() / 12) * 360 + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`; // <- backticks
 }
 
 // Run the setDate() function every 1000ms (one second). Think of as game loop.
